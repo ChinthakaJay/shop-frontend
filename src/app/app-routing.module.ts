83f195a71db628670277017aeb5331707e62d7a5
Chinthaka <chinthaka.jayarathne@axiatadigitallabs.com>
2021-06-22 22:02:53 +0530

    Add app page toolbar and buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PriceCalculatorComponent } from './components/price-calculator/price-calculator.component';
+import { PriceListComponent } from './components/price-list/price-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'price-list', component: PriceListComponent },
+    { path: 'price-calculator', component: PriceCalculatorComponent },
+    { path: '**', redirectTo: 'price-list'}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { PriceListComponent } from './components/price-list/price-list.component';
 import { PriceCalculatorComponent } from './components/price-calculator/price-calculator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

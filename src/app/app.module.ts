2c2bc76e54ff3b633269be98907e7c3f6ab09986
Chinthaka <chinthaka.jayarathne@axiatadigitallabs.com>
2021-06-22 21:31:47 +0530

    Initial message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PriceListComponent } from './components/price-list/price-list.component';
+import { PriceCalculatorComponent } from './components/price-calculator/price-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PriceListComponent,
+    PriceCalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
